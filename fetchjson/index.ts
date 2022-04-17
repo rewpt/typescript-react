331151c8491888c9e46b12b8f443a4c5c5835173
rewpt <zpermack@gmail.com>
2022-04-16 23:35:24 -0400

    refactor with typescript check for Todo

--- fetchjson/index.ts
+++ fetchjson/index.ts
@@ -2,14 +2,22 @@ import axios from 'axios';
 
 const url = 'https://jsonplaceholder.typicode.com/todos/1'
 
+
+interface Todo {
+  id: number;
+  title: string;
+  completed: boolean;
+}
+
 axios.get(url).then(response => {
-  const todo = (response.data);
-  const ID = todo.ID;
-  const title = todo.Title;
-  const finished = todo.finished;
+
+  const todo = (response.data) as Todo;
+  const ID = todo.id;
+  const title = todo.title;
+  const completed = todo.completed;
   console.log(`
     The Todo with ID: ${ID}
     Has a title of ${title}
-    Is it Finished? ${finished}
+    Is it Finished? ${completed}
   `)
 });
