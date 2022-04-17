396b633e28d60d1ebb1bd7588bf00b46b13bf9c7
rewpt <zpermack@gmail.com>
2022-04-17 15:54:47 -0400

    create typescript example for Objects and and classes

--- features/variables.ts
+++ features/variables.ts
@@ -6,3 +6,21 @@ let nothing: undefined = undefined;
 
 // JS built in objects
 let now: Date = new Date();
+
+// Array -> the left side indicated type and the right side is the array assigned to the var
+let colors: string[] = ['red', 'green', 'blue']
+let myNums: number[] = [1,2,3];
+let myBools: boolean[] = [true, false, true];
+
+// Classes
+class Car {
+
+}
+
+let car: Car = new Car();
+
+// Object literal, show what you are assigning and then the type of that assignment
+let point: { x: number; y: number} = {
+  x: 10,
+  y: 20,
+}
