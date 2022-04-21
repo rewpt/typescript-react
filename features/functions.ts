7eee87f1d707ed8a1deb9e2b883f3db754f0e447
rewpt <zpermack@gmail.com>
2022-04-21 16:28:46 -0400

    create subtract function example of not returning anything and not specifying type of return

--- features/functions.ts
+++ features/functions.ts
@@ -3,3 +3,11 @@
 const add = (a: number, b: number): number => {
   return a + b;
 }
+
+// We should always specify our return type to avoid errors like
+// not returning anything in our function;
+// for example this function won't throw any error because
+// it's not returning anything and we haven't specified a return type
+const subtract = (a:number, b:number) => {
+  a - b;
+}
