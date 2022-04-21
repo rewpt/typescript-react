d6e2a67df9ef4fc4d9340057e6d757516219a318
rewpt <zpermack@gmail.com>
2022-04-21 15:43:43 -0400

    add 3 examples of needing to explicity type annotate

--- features/annotation_rules.ts
+++ features/annotation_rules.ts
@@ -1,9 +1,33 @@
 // We will only have to use these annotation if:
 // 1) Declare a var on one line and initialize on another
-
+let declaredVar;
+declaredVar = 55;
 
 
 // 2) When we want a variable to have a type that can't be inferred
+let numbers = [-10, -1, 12];
+let numberAboveZero: boolean | number = false;
+
+for (let i = 0; i < numbers.length; i++) {
+  if (numbers[i] > 0) {
+    numberAboveZero = numbers[i];
+  }
+}
+
+
+
+
+
+let words = ['red', 'green', 'blue'];
+
+// will have an any type unless I you add in some type annotation
+let foundWord: boolean;
+
+for (let i = 0; i < words.length; i++) {
+  if (words[i] === 'green') {
+    foundWord = true;
+  }
+}
 
 
 // 3) When a function returns the 'any' type and we need to clarify the value
