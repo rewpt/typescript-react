8ebb8a7dcb43daf61fdf8c856af5b3107457a9b8
rewpt <zpermack@gmail.com>
2022-04-22 12:27:53 -0400

    examples of object destructuring with type annotations

---
+++ features/objects.ts
@@ -0,0 +1,21 @@
+const profile = { 
+  name: 'alex',
+  age: 20,
+  coords: { 
+    lat: 0,
+    lng: 15
+  },
+  setAge(age: number): void {
+    this.age = age;
+  }
+
+};
+
+// example of object destructuring and how we specify type
+// You mirror the structure because if you were destructuring
+// multiple variables it would not make sense without this
+const { age }: {age: number} = profile
+
+const { 
+  coords: {lat, lng}}: 
+{ coords : {lat: number, lng: number}} = profile;
