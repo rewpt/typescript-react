6479ad5c5b9fa11cce4d1ec67f3191b29e6a7c81
rewpt <zpermack@gmail.com>
2022-04-23 18:27:29 -0400

    create interface for print vehicle

---
+++ features/interfaces.ts
@@ -0,0 +1,32 @@
+// Interfaces create a new type, describing the property
+// names and value types of an object
+
+const oldCivic = {
+  name: 'civic',
+  year: 2000,
+  broken: true
+};
+// Here we have a type annotation that is getting pretty long
+// So we're going to make an interface so we don't have to duplicate
+// or make long annotations
+const printVehicle = (vehicle: { name: string; year: number; broken: boolean }): void => {
+  console.log(`Name: ${vehicle.name}`)
+  console.log(`Year: ${vehicle.year}`)
+  console.log(`broken: ${vehicle.broken}`)
+}
+
+printVehicle(oldCivic);
+
+// Here is an interface, we want them generic like vehicle
+// This is like a variable that refers to a type
+interface Vehicle {
+  name: string;
+  year: number;
+  broken: boolean;
+}
+
+const printVehicle22 = (vehicle: Vehicle): void => {
+  console.log(`Name: ${vehicle.name}`)
+  console.log(`Year: ${vehicle.year}`)
+  console.log(`broken: ${vehicle.broken}`)
+}
