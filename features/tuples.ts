de3b1a0e4bc9ebd45fff3bd40edfcee604b0709a
rewpt <zpermack@gmail.com>
2022-04-23 17:28:34 -0400

    create basic tuple example

---
+++ features/tuples.ts
@@ -0,0 +1,4 @@
+// In a tuple we put our values in a specific order to represent one thing
+// If this is a drink we are thinking color, carbonated, sugar content
+
+const drink = ['brown', true, 40]
