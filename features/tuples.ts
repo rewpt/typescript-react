be58a28b3a83a91ab901d8c482129e1c7ce053f2
rewpt <zpermack@gmail.com>
2022-04-23 17:32:39 -0400

    add tuple type examples

--- features/tuples.ts
+++ features/tuples.ts
@@ -1,4 +1,14 @@
 // In a tuple we put our values in a specific order to represent one thing
 // If this is a drink we are thinking color, carbonated, sugar content
+// we have to memorize the order and not screw it up
+const drink = ['brown', true, 40];
 
-const drink = ['brown', true, 40]
+// This is the drink represented in a class
+const drinkObj = {
+  color: 'brown',
+  carbonated: true,
+  sugar: 40
+}
+
+//Annotated tuple
+const pepsi: [string, boolean, number] = ['brown', true, 40];
