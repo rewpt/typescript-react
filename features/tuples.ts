dbec73f423da2388ef69d4b151143f2eb6feb626
rewpt <zpermack@gmail.com>
2022-04-23 17:35:37 -0400

    example of when to use vs tuples vs not

--- features/tuples.ts
+++ features/tuples.ts
@@ -11,4 +11,21 @@ const drinkObj = {
 }
 
 //Annotated tuple
+// Now we have some protection from having the wrong value in the wrong spot
 const pepsi: [string, boolean, number] = ['brown', true, 40];
+
+// If we have multiple things we want to fit this we can
+// make a type annotation for the array like so:
+// Now we can make multiple drinks of this type
+type Drink1 = [string, boolean, number];
+const sprite: Drink1 = ['clear', true, 50];
+
+// The most common time to use a tuple is representing a single
+// row of a CSV file
+// However we don't use tuples often because they're very unclear
+const carSpecs: [number, number]= [400, 3354];
+// This is much more obvious at a glance of what we're talking about
+const carStats = {
+  horsepower: 400,
+  weight: 3354
+}
