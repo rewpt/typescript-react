63ea2f57091a206cbae43285d6763972fdffda58
rewpt <zpermack@gmail.com>
2022-04-24 11:59:44 -0400

    add notes on public, private, protected classes

--- features/classes.ts
+++ features/classes.ts
@@ -1,3 +1,8 @@
+//Public methods: can be called anywhere any time
+// Private methods: Can only be called by other methods in this class
+// Protected methods: can be called by other
+//                    methods in this class or by other methods in child classes
+
 class Vehicle {
   drive(): void {
     console.log('chug chug');
