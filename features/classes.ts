329cc3870fcf7150637e88efa7e3a4c2c1297eef
rewpt <zpermack@gmail.com>
2022-04-24 12:05:27 -0400

    examples of private and protected methods in classes

--- features/classes.ts
+++ features/classes.ts
@@ -2,21 +2,30 @@
 // Private methods: Can only be called by other methods in this class
 // Protected methods: can be called by other
 //                    methods in this class or by other methods in child classes
+// Methods are not marked private for security
+// Private methods are only used to prevent other developers from calling it
+// incorrectly. It may manipulate an object in away that can break something. 
+
 
 class Vehicle {
-  drive(): void {
-    console.log('chug chug');
-  }
-  honk(): void {
+  // honk can be used in Auto because it's protected (not private)
+  protected honk(): void {
     console.log('beep');
   }
 }
 
 class Auto extends Vehicle  {
-  drive(): void {
+  // a private method must have continuity of modifiers by having it be private in
+  // both the parent and child if it exists in parent and is overridden in the child
+  private drive(): void {
     console.log('vroom')
+  }
+
+  startDrivingProcess(): void {
+    this.drive();
+    this.honk();
   }
 }
 
 const car3 = new Auto();
-car3.drive();
+car3.startDrivingProcess();
